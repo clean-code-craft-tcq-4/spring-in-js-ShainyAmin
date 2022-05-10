56e2ef7b5a73ef338f8dd7de6eccec13fdd058a1
ShainyAmin <38464281+ShainyAmin@users.noreply.github.com>
2022-05-10 19:29:32 +0530

    Update statistics.js

--- app/statistics.js
+++ app/statistics.js
@@ -4,15 +4,27 @@ module.exports=function getStatistics(numbers){
     var min = numbers[0];
     var max = numbers[0];
     var total = 0;
-    numbers.forEach((ele) => {
+  if(numbers.length != 0) {
+        numbers.forEach((ele) =>
+     {
                     if(ele > max){
                         max = ele;
                          }
+                        
+                        
                     if(ele < min) {
                     min = ele;
                     }
+                   
                    total += ele;
                     })
+
+    }
+    else {
+        max = NaN;
+        min = NaN;
+        }
+    
      var average = total/(numbers.length);
      return {"min": min,
             "max": max,
