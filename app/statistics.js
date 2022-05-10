b1c5f73b6c9f29a1097e206e1cf6c11a2ce27a6d
ShainyAmin <38464281+ShainyAmin@users.noreply.github.com>
2022-05-10 16:04:43 +0530

    Update statistics.js

--- app/statistics.js
+++ app/statistics.js
@@ -1,5 +1,20 @@
 module.exports=function getStatistics(numbers){
     
     //implement the computation of statistics here
+    var min = numbers[0];
+    var max = numbers[0];
+    var total = 0;
+    numbers.forEach(funtion(ele) {
+                    if(ele > max){
+                        max = ele;
+                         }
+                    if(ele > min) {
+                    min = ele;
+                    }
+                   total += ele;
+                    })
+     var average = total/(numbers.length);
+     return [max, min, average];
+
 }
 
