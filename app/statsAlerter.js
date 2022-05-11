64a64bea449f7c17a3182bec7d529c17397e5ab0
ShainyAmin <38464281+ShainyAmin@users.noreply.github.com>
2022-05-11 20:54:05 +0530

    Update statsAlerter.js

--- app/statsAlerter.js
+++ app/statsAlerter.js
@@ -1,1 +1,19 @@
+ module.exports = {  StatsAlerter: StatsAlerter }
 
+function StatsAlerter(maxThreshold,alerters){
+   
+        this.maxThreshold = maxThreshold;
+        this.alerters = alerters;
+       
+    
+         function checkAndAlert(numbers){
+            
+             numbers.forEach((num) =>{
+             if( num > this.maxThreshold) {
+                this.alerters[0].emailSent();
+                this.alerters[1].ledGlows();
+             }
+             })
+            
+        }
+    }
